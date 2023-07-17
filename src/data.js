9be826b5eba1d194fbb3dd2f2a3a3179fb7aba8f
vijay <VijayIyer>
2023-07-17 11:56:36 -0400

    creating data file for transactions

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const transactions = [
+  {
+    customer: "A",
+    amount: 100,
+    date: "07/01/2023",
+  },
+  {
+    customer: "B",
+    amount: 150,
+    date: "07/15/2023",
+  },
+  {
+    customer: "A",
+    amount: 176,
+    date: "07/20/2023",
+  },
+  {
+    customer: "C",
+    amount: 105,
+    date: "07/25/2023",
+  },
+  {
+    customer: "C",
+    amount: 200,
+    date: "08/20/2023",
+  },
+  {
+    customer: "A",
+    amount: 80,
+    date: "08/29/2023",
+  },
+  {
+    customer: "B",
+    amount: 135,
+    date: "09/05/2023",
+  },
+  {
+    customer: "C",
+    amount: 65,
+    date: "09/28/2023",
+  },
+  {
+    customer: "A",
+    amount: 55,
+    date: "09/29/2023",
+  },
+];
