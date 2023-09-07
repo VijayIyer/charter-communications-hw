4bde3595bd84ab2675e4538635e242357d3d7f6f
vijay <VijayIyer>
2023-09-06 20:02:15 -0400

    Modifies getUniqueElements method to use more efficient and cleaner code

--- src/utils/getUniqueElements.js
+++ src/utils/getUniqueElements.js
@@ -1,9 +1,3 @@
 export const getUniqueElements = (arr) => {
-  let outputArray = arr.filter(function (v, i, self) {
-    // It returns the index of the first
-    // instance of each value
-    return i === self.indexOf(v);
-  });
-
-  return outputArray;
+  return [...new Set(arr)];
 };
