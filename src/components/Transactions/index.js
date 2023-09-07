9a52f271561a637379e880ff0b3d811dd26ee448
vijay <VijayIyer>
2023-09-06 20:12:22 -0400

    Removes commented out code

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -1,13 +1,11 @@
 import TransactionsTable from "./TransactionsTable";
 import MonthFilter from "./MonthFilter";
 import Search from "./Search";
-import Regenerate from "./Regenerate";
 import "./styles.css";
 export default function Transactions() {
   return (
     <div className='column'>
       <h3 className='transactions__header'>Transactions</h3>
-      {/* <Regenerate /> */}
       <div className='transactions__filters'>
         <h3>Filters</h3>
         <MonthFilter />
